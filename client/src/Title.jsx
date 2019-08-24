8798b583bacd49c50759f0252d34a6423e37ff5a
Whitney Lee <whitneyleephotography@gmail.com>
2019-08-23 19:52:24 -0500

    Render title component

---
+++ client/src/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = (props) => {
+  return (
+    <div id="des_title">
+      {props.item.title}
+    </div>
+  )
+};
+
+export default Title;
--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Title from './Title.jsx';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      selectedItem: {},
 
     }
   }
 
+  componentDidMount() {
+    axios.get('/item')
+    .then((response) => {
+      this.setState({
+        magic: response.data,
+        selectedItem: response.data[0]
+      }, () => console.log(this.state.selectedItem))
+    })
+  }
+
   render() {
     return (
-      <div>Hello from React!</div>
+      <Title item = {this.state.selectedItem}/>
+
     )
   }
 
