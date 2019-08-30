3e06b8b712dbec1ecd62caf3f6d5a66180369c10
Whitney Lee <whitneyleephotography@gmail.com>
2019-08-30 18:24:34 -0500

    Add deployment config file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'module-item-description',
+    script: 'server/server.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-219-43-62.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/FEC.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/shazamazon/module-item-description',
+      path: '/home/ubuntu/module-item-description',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
