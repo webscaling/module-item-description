c2cedffeb58713c2a5f55f8665ae73538ada19fd
Whitney Lee <whitneyleephotography@gmail.com>
2019-08-30 19:03:11 -0500

    Tinkering with file paths

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [{
     name: 'module-item-description',
-    script: 'module-item-description/server.js'
+    script: './server/server.js'
   }],
   deploy: {
     production: {
       user: 'ubuntu',
       host: 'ec2-18-219-43-62.us-east-2.compute.amazonaws.com',
-      key: '.ssh/FEC.pem',
+      key: '~/.ssh/tutorial-2.pem',
       ref: 'origin/master',
-      repo: 'https://github.com/shazamazon/module-item-description',
-      path: '/module-item-description',
+      repo: 'https://github.com/shazamazon/module-item-description.git',
+      path: '/home/ubuntu/module-item-description',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
