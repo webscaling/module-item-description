d77f1a9fffe3818a4d522b1f5700793751b9ed63
Whitney Lee <whitneyleephotography@gmail.com>
2019-09-04 15:38:49 -0500

    Add 'display: inline-block' to img border html in hopes that will make it show up

--- client/src/Price_Over.jsx
+++ client/src/Price_Over.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 
 const Price_Over = (props) => {
-  console.log(props)
   return (
     <div id="des_price">
     Price: <span id="des_money">${parseFloat(props.item.Price).toFixed(2)}</span> <span className="des_bold">FREE Delivery by Instant Manifestation. </span> <span className="des_blue">Details</span> & <span className="des_blue">FREE Returns</span>
