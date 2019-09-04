43e719c36f9269e9b2a3411585f15a93857b0ee0
Whitney Lee <whitneyleephotography@gmail.com>
2019-09-04 15:07:20 -0500

    Change wording

--- client/src/Price_Over.jsx
+++ client/src/Price_Over.jsx
@@ -5,7 +5,7 @@ const Price_Over = (props) => {
   console.log(props)
   return (
     <div id="des_price">
-    Price: <span id="des_money">${parseFloat(props.item.Price).toFixed(2)}</span> <span className="des_bold">FREE Delivery by Owl. </span> <span className="des_blue">Details</span> & <span className="des_blue">FREE Returns</span>
+    Price: <span id="des_money">${parseFloat(props.item.Price).toFixed(2)}</span> <span className="des_bold">FREE Delivery by Instant Manifestation. </span> <span className="des_blue">Details</span> & <span className="des_blue">FREE Returns</span>
     </div>
   )
 }
